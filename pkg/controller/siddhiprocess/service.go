cda911b092d87cd3ae5a74a1252970bc86e6dd45
Buddhi Kothalawala <buddhi@Buddhis-MacBook-Pro.local>
2019-04-12 01:29:25 +0530

    Add initial stateless siddhi-operator

---
+++ pkg/controller/siddhiprocess/service.go
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 WSO2 Inc. (http:www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package siddhiprocess
+
+import(
+	"strconv"
+	"strings"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	siddhiv1alpha1 "github.com/siddhi-io/siddhi-operator/pkg/apis/siddhi/v1alpha1"
+
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+// serviceForSiddhi returns a Siddhi Service object
+func (reconcileSiddhiProcess *ReconcileSiddhiProcess) serviceForSiddhiProcess(siddhiProcess *siddhiv1alpha1.SiddhiProcess, siddhiApp SiddhiApp, operatorEnvs map[string]string) *corev1.Service {
+	labels := labelsForSiddhiProcess(siddhiProcess.Name, operatorEnvs)
+	var servicePorts []corev1.ServicePort
+	for _, port := range siddhiApp.Ports{
+		servicePort := corev1.ServicePort{
+			Port: int32(port),
+			Name: strings.ToLower(siddhiApp.Name) + strconv.Itoa(port),
+		}
+		servicePorts = append(servicePorts, servicePort)
+	}
+	service := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Service",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      siddhiProcess.Name,
+			Namespace: siddhiProcess.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: labels,
+			Ports: servicePorts,
+			Type: "ClusterIP",
+		},
+	}
+	controllerutil.SetControllerReference(siddhiProcess, service, reconcileSiddhiProcess.scheme)
+	return service
+}
